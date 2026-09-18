53135e25b14dba0b500a3a3e67bcc7015c48d38f
lampink <phlam189@gmail.com>
2026-09-18 18:28:52 +0700

    Add return type novels

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -30,7 +31,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-    public function novels()
+    public function novels(): HasMany
     {
         return $this->hasMany(Novel::class, 'uploader_id');
     }
